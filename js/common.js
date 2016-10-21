dbda092f02a8851a5676e43d8572c84c4051f6ac
whiteWork <dupili2009@163.com>
2016-10-21 09:59:29 +0800

    头部

--- js/common.js
+++ js/common.js
@@ -1,3 +1,52 @@
+/*window.onload = function(){
+	//回到顶部
+	(function(){
+		if($(window).scrollTop()>200){
+			$('#go_top').show()
+		}else{
+			$('#go_top').hide()
+		}
+		$(window).scroll(function(){
+			if($(window).scrollTop()>200){
+				$('#go_top').show()
+			}else{
+				$('#go_top').hide()
+			}
+		});
+		$('#go_top').on('click',function(){
+			$('body,html').animate({scrollTop:0},600); 
+			return false;
+		});
+	})();
+
+
+}*/
+
+
+//回到顶部
+function goTop(obj){
+
+	if($(window).scrollTop()>200){
+		$(obj).show()
+	}else{
+		$(obj).hide()
+	}
+	$(window).scroll(function(){
+		if($(window).scrollTop()>200){
+			$(obj).show()
+		}else{
+			$(obj).hide()
+		}
+	});
+	$(obj).on('click',function(){
+		$('body,html').animate({scrollTop:0},600); 
+		return false;
+	});
+}
+addEvent(window,'load',function(){
+	document.body.addEventListener('touchstart', function () { });
+})
+
 /*获取class*/
 function getByClass(oParent,sClass){
 	if(oParent.getElementsByClassName){
@@ -73,6 +122,8 @@ function toDou(iNum){
 		return iNum;
 	}
 }
+
+//提示弹窗
 function tishi(txt,time,fn){
 	var oDiv = document.createElement('div');
 	time = time||3000;
@@ -91,3 +142,82 @@ function tishi(txt,time,fn){
 }
 
 
+
+//运动开始
+function getStyle(obj,name){
+	return (obj.currentStyle || getComputedStyle(obj,false))[name];
+}
+function move(obj,json,options){
+	//考虑默认值
+	options=options || {};
+	options.duration=options.duration || 300;
+	options.easing=options.easing || 'ease-out';
+	
+	var count=Math.round(options.duration/30);
+	var start={};
+	var dis={};
+	for(var name in json){
+		start[name]=parseFloat(getStyle(obj,name));
+		if(isNaN(start[name])){
+			switch(name){
+				case 'left':
+					start[name]=obj.offsetLeft;
+					break;
+				case 'top':
+					start[name]=obj.offsetTop;
+					break;
+				case 'width':
+					start[name]=obj.offsetWidth;
+					break;
+				case 'height':
+					start[name]=obj.offsetHeight;
+					break;
+				case 'marginLeft':
+					start[name]=obj.offsetLeft;
+					break;
+				case 'borderWidth':
+					start[name]=0;
+					break;
+				//...
+			}
+		}
+		dis[name]=json[name]-start[name];
+	}
+	
+	var n=0;
+	
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+		n++;
+		
+		for(var name in json){
+			switch(options.easing){
+				case 'linear':
+					var a=n/count;
+					var cur=start[name]+dis[name]*a;
+					break;
+				case 'ease-in':
+					var a=n/count;
+					var cur=start[name]+dis[name]*a*a*a;
+					break;
+				case 'ease-out':
+					var a=1-n/count;
+					var cur=start[name]+dis[name]*(1-a*a*a);
+					break;
+			}
+			
+			if(name=='opacity'){
+				obj.style.opacity=cur;
+				obj.style.filter='alpha(opacity:'+cur*100+')';
+			}else{
+				obj.style[name]=cur+'px';
+			}
+		}
+		
+		if(n==count){
+			clearInterval(obj.timer);
+			options.complete && options.complete.call(obj);
+		}
+	},30);
+}
+//运动结束
